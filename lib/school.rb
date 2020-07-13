f78ff4a0e0bfa595c75691fa2dcd699521ecfb60
Adi Skop <adiskop@gmail.com>
2020-07-13 13:42:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,10 @@ def initialize(name)
 end
 
 def add_student(name,grade)
-   roster[grade] ||= []
-  roster[grade] << name
+   if roster[grade]
+     roster[grade] << name
+   else roster[grade]=[name]
+   end
 end
 
 def grade(grade)
